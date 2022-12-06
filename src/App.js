aa3f670086019432c7efb32279289aa9e4fc444b
manahiang <manahiang98@gmial.com>
2022-12-06 16:53:13 +0700

    body

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Header from './components/header';
+import './index.css';
+
+function App() {
+  return (
+    <div>
+       <Header/>
+    </div>
+  );
+}
+
+export default App;
